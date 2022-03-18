8a25530f7b07c870300684202336b6e4ff54647c
song4 <91song4@daum.net>
2022-03-18 21:58:23 +0900

    상속관계에 있는 예외클래스

---
+++ 열혈강의 C++/ATMSim2.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+class AccountException
+{
+public:
+	virtual void ShowExceptionReason() = 0;
+};
+
+class DepositException :public AccountException
+{
+private:
+	int reqDep; // 요청 입금액
+
+public:
+	void ShowExceptionReason()
+	{
+		cout << "[예외 메시지: " << reqDep << "는 입금불가]" << endl;
+	}
+
+public:
+	DepositException(int money)
+		: reqDep(money)
+	{}
+};
+
+class WithdrawException :public AccountException
+{
+private:
+	int balance; // 잔고
+
+public:
+	void ShowExceptionReason()
+	{
+		cout << "[예외 메시지: 잔액" << balance << ", 잔액부족]" << endl;
+	}
+
+public:
+	WithdrawException(int money)
+		: balance(money)
+	{}
+};
+
+class Account
+{
+private:
+	char accNum[50];
+	int balance; // 잔고
+
+public:
+	void Deposit(int money) throw(AccountException)
+	{
+		if (money < 0)
+		{
+			DepositException expn(money);
+			throw expn;
+		}
+		balance += money;
+	}
+
+	void Withdraw(int money) throw(AccountException)
+	{
+		if (money > balance)
+			throw WithdrawException(balance);
+		balance -= money;
+	}
+
+	void ShowMyMoney()
+	{
+		cout << "잔고: " << balance << endl << endl;
+	}
+
+public:
+	Account(const char* acc, int money)
+		: balance(money)
+	{
+		strcpy_s(accNum, acc);
+	}
+};
+
+int main(void)
+{
+	Account myAcc("56789-827120", 5000);
+
+	try
+	{
+		myAcc.Deposit(2000);
+		myAcc.Deposit(-300);
+	}
+	catch (AccountException& expn)
+	{
+		expn.ShowExceptionReason();
+	}
+	myAcc.ShowMyMoney();
+
+	try
+	{
+		myAcc.Withdraw(3500);
+		myAcc.Withdraw(4500);
+	}
+	catch (AccountException& expn)
+	{
+		expn.ShowExceptionReason();
+	}
+	myAcc.ShowMyMoney();
+	return 0;
+}
